ecb01e14cd2f711e842e85aca2b080ae6c50e41a
manutg2209 <manutg2209@gmail.com>
2013-02-27 21:28:19 +0000

    Creando BimestreNegocio

---
+++ omq/colegio/negocio/BimestreNegocio.php
@@ -0,0 +1,51 @@
+<?php
+require_once '../dao/BimestreDAO.php';
+require_once '../modelo/Bimestre.php';
+
+class BimestreNegocio{
+        
+        public function insertar ($id_bimestre, $nombre, $grado){
+                
+                $modelo = new Bimestre();
+                $modelo->setId_cursos($id_bimestre);
+                $modelo->setNombre($nombre);
+                $modelo->setDetalle($grado);
+                
+                $dao = new BimestreDAO();
+                $dao->insertar($modelo);
+        }
+
+        public function listar(){
+                $dao = new BimestreDAO();            
+                return $dao->listar();
+        }
+        
+        public function obtener($id_bimestre){
+                
+                $modelo = new Bimestre();
+                $modelo->setId_cursos($id_bimestre);
+                
+                $dao = new BimestreDAO();
+                return $dao->obtener($modelo);
+        }
+        
+        public function modificar($id_bimestre, $nombre){
+                
+                $modelo = new Bimestre();
+                $modelo->setId_bimestre($id_bimestre);
+                $modelo->setNombre($nombre);
+                
+                $dao = new BimestreDAO();
+                $dao->modificar($modelo);
+        }
+        
+        public function eliminar($id_bimestre){
+                
+                $modelo = new Bimestre();
+                $modelo->setId_bimestre($id_bimestre);
+                
+                $dao = new BimestreDAO();
+                $dao->eliminar($modelo);
+        }
+}
+?>
