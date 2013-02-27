047eb81bfc8b73e80846411939be04df7257f185
manutg2209 <manutg2209@gmail.com>
2013-02-27 17:11:28 +0000

    Cambios en Usuario_RolNegocio.php

--- omq/colegio/negocio/Usuario_RolNegocio.php
+++ omq/colegio/negocio/Usuario_RolNegocio.php
@@ -1,5 +1,50 @@
 <?php
+require_once '../dao/Usuario_RolDAO.php';
+require_once '../modelo/Usuario_Rol.php';
 
+class Usuario_RolNegocio{
+        
+        public function insertar ($rol_nombre,$id_usuario){
+                
+                $modelo = new Usuario_Rol();
+                $modelo->setRol_nombre($rol_nombre);
+                $modelo->setId_usuario($id_usuario);
+                
+                $dao = new Usuario_RolDAO();
+                $dao->insertar($modelo);
+        }
 
-
+        public function listar(){
+                $dao = new Usuario_RolDAO();            
+                return $dao->listar();
+        }
+        
+        public function obtener($id_usuario){
+                
+                $modelo = new Usuario_Rol();
+                $modelo->setId_usuario($id_usuario);
+                
+                $dao = new Usuario_RolDAO();
+                return $dao->obtener($modelo);
+        }
+        
+        public function modificar($rol_nombre, $id_usuario){
+                
+                $modelo = new Usuario_Rol();
+                $modelo->setRol_nombre($rol_nombre);
+                $modelo->setId_usuario($id_usuario);
+                
+                $dao = new Usuario_RolDAO();
+                $dao->modificar($modelo);
+        }
+        
+        public function eliminar($id_usuario){
+                
+                $modelo = new Usuario_Rol();
+                $modelo->setId_usuario($id_usuario);
+                
+                $dao = new Usuario_RolDAO();
+                $dao->eliminar($modelo);
+        }
+}
 ?>
